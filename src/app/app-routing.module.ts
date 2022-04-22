2012da1daf640d3fa12a03cbdb5aaca71f35cf57
Cintia <cintialeal31m@gmail.com>
2022-04-22 20:53:22 -0300

    primeros formularios para cliente y productos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormClientComponent } from './form-client/form-client.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'clientes', component: FormClientComponent},
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
